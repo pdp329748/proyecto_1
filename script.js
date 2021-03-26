021160e11b7e1b6065067738cbb7d5cbb2427583
a329748 <a329748@uach.mx>
2021-03-26 00:36:36 -0700

    Final?
    Will test overnight, might change some stuff later.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 var imgOn = "assets/table-on.png";
 var imgOff = "assets/table-off.png";
 
+var startTime = "";
+
 function toggle(id, source) {
-	var timerBool = (source.endsWith(imgOn) ? true : false);
+	var timerBool = (source.endsWith(imgOff) ? true : false);
 	document.getElementById(id).src = (source.endsWith(imgOff) ? imgOn : imgOff);
 	if(timerBool) {
-		alert(new Date());
 		var start = new Date() + "";
-		var startTime = start.substr((start.indexOf(":") - 2), 8);
+		startTime = start.substr((start.indexOf(":") - 2), 8);
 		var startId = "start-" + id.substr(id.length - 1);
 		document.getElementById(startId).innerHTML = "Hora inicial: " + startTime;
 	} else {
@@ -17,10 +18,29 @@ function toggle(id, source) {
 		var endId = "end-" + id.substr(id.length - 1);
 		document.getElementById(endId).innerHTML = "Hora final: " + elapsedTime;
 		var totalId = "total-" + id.substr(id.length - 1);
-		document.getElementById(endId).innerHTML = "Total: $" + (hours*50.00);
+		document.getElementById(totalId).innerHTML = "Total: $" + (hours*50.00);
 	}
 }
 
-function timeDiff(start, end) {
+setInterval(refresh, 60000);
 
+function refresh() {
+
+}
+
+function timeDiff(start, end) {
+	alert(start + " " + end);
+	var startHr = parseInt(start.substr(0, 2));
+	var startMin = parseInt(start.substr(4, 2));
+	var startSec = parseInt(start.substr(7, 2));
+	var startTotal = startHr*60*60 + startMin*60 + startSec;
+	var endHr = parseInt(start.substr(0, 2));
+	var endMin = parseInt(end.substr(4, 2));
+	var endSec = parseInt(end.substr(7, 2));	
+	var endTotal = endHr*60*60 + endMin*60 + endSec;
+	if(endTotal < startTotal) {
+		endTotal += 1440;
+	}
+	let hours = Math.floor((endTotal - startTotal) / 3600) + 1;
+	return hours;
 }
